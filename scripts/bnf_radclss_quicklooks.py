f8cb5966745e82133985ca943d64fe32908d9577
jrobrien91 <obrienj@compute-386-06.cels.anl.gov>
2025-06-13 16:08:50 -0500

    ADD: Quicklook generation script

--- scripts/bnf_radclss_quicklooks.py
+++ scripts/bnf_radclss_quicklooks.py
@@ -6,10 +6,12 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 import glob
 import sys
+import traceback
 import time
 import datetime
 import argparse
 import dask
+from datetime import timedelta
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,12 +23,142 @@ dask.config.set({'logging.distributed': 'error'})
 
 from matplotlib.dates import DateFormatter
 from matplotlib import colors
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import pyart
+import act
 
 #-----------------
 # Define Functions
 #-----------------
+def create_radclss_columns(radclss,
+                           field="corrected_reflectivity",
+                           p_vmin=-5,
+                           p_vmax=65,
+                           outdir="./"):
+    """
+    With the RadCLss product, generate a figure of all extracted columns
+
+    Input
+    -----
+    radclss : str
+        Filepath to the RadCLss file.
+    field : str
+        Specific CMAC field to display extracted column of
+    vmin : int
+        Minimum value to display between all subplots for the specific radar
+        parameter
+    vmax : int
+        Maximum value to display between all subplots for the specific radar
+        parameter
+    outdir : str
+        Path to desired output directory. If not supplied, assumes current 
+        working directory.
+
+    Output
+    ------
+    timeseries : png
+        Saved image of the RadCLss timeseris
+    
+    """
+    # Create the figure
+    fig, axarr = plt.subplots(3, 2, figsize=(14, 10))
+    plt.subplots_adjust(hspace=0.8)
+
+    # read the RadCLss file
+    try:
+        ds = xr.open_dataset(radclss[0], decode_timedelta=False)
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        # 'e' will contain the error object
+        print("\nERROR - (create_radclss_timeseries):" +
+              f" \n\tOccured When Reading in RadCLss File: \n\t{e}")
+        print(f"\tError type: {type(e)}")
+        print(f"\tLine Number: ", exc_tb.tb_lineno)
+        print(f"\tFile Name: ", exc_tb.tb_frame.f_code.co_filename)
+        print("\n")
+        return
+    
+    # Define the time of the radar file we are plotting against
+    radar_time = datetime.datetime.strptime(np.datetime_as_string(ds['time'].data[0], unit="s"), 
+                                            "%Y-%m-%dT%H:%M:%S")
+    final_time = radar_time + timedelta(days=1)
+
+    ds[field].sel(station="M1").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                        y="height", 
+                        ax=axarr[0, 0], 
+                        vmin=p_vmin, 
+                        vmax=p_vmax, 
+                        cmap="ChaseSpectral"
+    )
+    ds[field].sel(station="S4").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                       y="height", 
+                       ax=axarr[0, 1], 
+                       vmin=p_vmin, 
+                       vmax=p_vmax, 
+                       cmap="ChaseSpectral"
+    )
+    ds[field].sel(station="S20").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                       y="height", 
+                       ax=axarr[1, 0], 
+                       vmin=p_vmin, 
+                       vmax=p_vmax, 
+                       cmap="ChaseSpectral"
+    )
+    ds[field].sel(station="S30").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                       y="height", 
+                       ax=axarr[1, 1], 
+                       vmin=p_vmin, 
+                       vmax=p_vmax, 
+                       cmap="ChaseSpectral"
+    )
+    ds[field].sel(station="S40").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                       y="height", 
+                       ax=axarr[2, 0], 
+                       vmin=p_vmin, 
+                       vmax=p_vmax, 
+                       cmap="ChaseSpectral"
+    )
+    ds[field].sel(station="S13").sel(
+        time=slice(radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                   final_time.strftime("%Y-%m-%dT00:00:00"))).plot(
+                       y="height", 
+                       ax=axarr[2, 1], 
+                       vmin=p_vmin, 
+                       vmax=p_vmax, 
+                       cmap="ChaseSpectral"
+    )
+
+    # save the figure
+    try:
+        fig.savefig(outdir + 
+                    'bnf-radclss-columns.' + 
+                    radclss[0].split('.')[-3]+
+                    '.png')
+        plt.close(fig)
+        STATUS = "COLUMNS SUCCESS"
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        # 'e' will contain the error object
+        print("\nERROR - (create_radclss_timeseries):" +
+              f" \n\tOccured When Saving Figure to File: \n\t{e}")
+        print(f"\tError type: {type(e)}")
+        print(f"\tLine Number: ", exc_tb.tb_lineno)
+        print(f"\tFile Name: ", exc_tb.tb_frame.f_code.co_filename)
+        print("\n")
+        STATUS = "COLUMNS FAILED"
+
+    return STATUS
 
 def create_radclss_timeseries(radclss,
                               field="corrected_reflectivity", 
@@ -36,11 +168,12 @@ def create_radclss_timeseries(radclss,
                               rheight=750, 
                               outdir="./"):
     """
-    With the RadCLss product, generate a timeseries of radar reflectivity factor, 
-    particle size distribution and cumuluative precipitaiton 
+    With the RadCLss product, generate a timeseries of radar reflectivity 
+    factor, particle size distribution and cumuluative precipitaiton 
     for the ARM SAIL M1 Site. 
 
-    This timeseries quick is to serve as a means for evaluating the RadCLss product.
+    This timeseries quick is to serve as a means for evaluating the RadCLss 
+    product.
 
     Input
     -----
@@ -57,9 +190,11 @@ def create_radclss_timeseries(radclss,
     dis_site : str
         Identifer of the supported location for lat/lon slices
     height : int
-        Column height to compare against in-situ sensors for precipitation accumulation. 
+        Column height to compare against in-situ sensors for precipitation 
+        accumulation. 
     outdir : str
-        Path to desired output directory. If not supplied, assumes current working directory.
+        Path to desired output directory. If not supplied, assumes current 
+        working directory.
 
     Output
     ------
@@ -67,22 +202,33 @@ def create_radclss_timeseries(radclss,
         Saved image of the RadCLss timeseris
     """
     # Create the figure
-    fig = plt.figure(figsize=(16, 8))
-    print(radclss)
+    fig = plt.figure(figsize=(14, 10))
+    plt.subplots_adjust(wspace=0.05)
+
     # read the RadCLss file
     try:
         ds = xr.open_dataset(radclss[0], decode_timedelta=False)
     except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
         # 'e' will contain the error object
-        print(f"An error occurred: {e}")
-        print(f"Error type: {type(e)}")
+        print("\nERROR - (create_radclss_timeseries):" +
+              f" \n\tOccured When Reading in RadCLss File: \n\t{e}")
+        print(f"\tError type: {type(e)}")
+        print(f"\tLine Number: ", exc_tb.tb_lineno)
+        print(f"\tFile Name: ", exc_tb.tb_frame.f_code.co_filename)
+        print("\n")
         return
     
+    # Define the time of the radar file we are plotting against
+    radar_time = datetime.datetime.strptime(np.datetime_as_string(ds['time'].data[0], unit="s"), 
+                                            "%Y-%m-%dT%H:%M:%S")
+    final_time = radar_time + timedelta(days=1)
+
     #-----------------------------------------------
     # Side Plot A - Display the RadClss Radar Field
     #-----------------------------------------------
     # Top right hand subplot - Radar TimeSeries
-    ax2 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(311)
 
     ds[field].sel(station=dis_site).plot(x="time", 
                                          ax=ax2, 
@@ -91,7 +237,8 @@ def create_radclss_timeseries(radclss,
                                          vmax=vmax
     )
     
-    ax2.set_title("BNF Site: " + dis_site)
+    ax2.set_title("Extracted Radar Columns and In-Situ Sensors (RadCLss), BNF Site: " + 
+                  dis_site)
     ax2.set_ylabel("Height [m]")
     ax2.set_xlabel("Time [UTC]")
 
@@ -99,7 +246,7 @@ def create_radclss_timeseries(radclss,
     # Side Plot B - Display the Rain Rates
     #--------------------------------------
     # Top right hand subplot - Radar TimeSeries
-    ax3 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(312)
 
     # CMAC derived rain rate
     ds["rain_rate_A"].sel(station=dis_site).sel(
@@ -123,22 +270,24 @@ def create_radclss_timeseries(radclss,
         )
             
     ax3.set_title(" ")
-    ax3.set_ylabel("Precipitation Rate [mm/hr]")
+    ax3.set_ylabel("Precipitation Rate \n[mm/hr]")
     ax3.set_xlabel("Time [UTC]")
+    ax3.set_xlim([radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                  final_time.strftime("%Y-%m-%dT00:00:00")])
     ax3.legend(loc="upper right")
     ax3.grid(True)
     # Add a blank space next to the subplot to shape it as the above plot
     divider = make_axes_locatable(ax3)
-    cax = divider.append_axes("right", size="3%", pad=1.05)
+    cax = divider.append_axes("right", size="3%", pad=1.9)
     cax.set_visible(False)
 
     #------------------------------------------
     # Side Plot C - Precipitation Accumulation
     #------------------------------------------
-    ax4 = fig.add_subplot(133)
+    ax4 = fig.add_subplot(313)
    
     # CMAC Accumulated Rain Rates
-    radar_accum = act.utils.accumulate_precip(ds.sel(station=dis_site).isel(height=rheight), 'rain_rate_A').compute()
+    radar_accum = act.utils.accumulate_precip(ds.sel(station=dis_site).sel(height=rheight), 'rain_rate_A').compute()
     # CMAC Accumulated Rain Rates
     radar_accum['rain_rate_A_accumulated'].plot(x="time", 
                                                 ax=ax4,
@@ -153,6 +302,7 @@ def create_radclss_timeseries(radclss,
                                 label="PLUVIO2"
         )
 
+
     # LDQUANTS Accumulation
     if dis_site == "M1" or dis_site == "S30":
         ld_precip_accum = act.utils.accumulate_precip(ds.sel(station=dis_site), 'rain_rate').rain_rate_accumulated.compute()
@@ -162,37 +312,57 @@ def create_radclss_timeseries(radclss,
         )  
     
     ax4.set_title(" ")
-    ax4.set_ylabel("Accumulated Precipitation [mm]")
+    ax4.set_ylabel("Accumulated Precipitation \n[mm]")
     ax4.set_xlabel("Time [UTC]")
     ax4.legend(loc="upper left")
     ax4.grid(True)
-    ax4.set_ylim(0, radar_accum["rain_rate_A_accumulated"].max()+0.25)
+    ax4.set_ylim(0, radar_accum["rain_rate_A_accumulated"].max()+20)
+    ax4.set_xlim([radar_time.strftime("%Y-%m-%dT00:00:00"), 
+                  final_time.strftime("%Y-%m-%dT00:00:00")])
     # Add a blank space next to the subplot to shape it as the above plot
     divider = make_axes_locatable(ax4)
-    cax = divider.append_axes("right", size="3%", pad=1.05)
+    cax = divider.append_axes("right", size="3%", pad=1.9)
     cax.set_visible(False)
 
     # Set the DPI to a higher value (e.g., 300)
     plt.rcParams['figure.dpi'] = 300
     plt.rcParams['savefig.dpi'] = 300
 
+    # Add the title
+    ##plt.suptitle("BNF Extracted Radar Columns and In-Situ Sensors (RadCLss) \n" + 
+    ##             radar_time.strftime("%Y-%m-%d"))
+
     # save the figure
     try:
-        fig.savefig(out_dir + 
-                    'bnf-radclss-.' + 
-                    radclss.split('.')[-2]+
+        fig.savefig(outdir + 
+                    'bnf-radclss-timeseries.' +
+                    dis_site + 
+                    '.' + 
+                    radclss[0].split('.')[-3]+
                     '.png')
         plt.close(fig)
-        STATUS = True
-    except:
-        STATUS = False
+        STATUS = "TIMESERIES SUCCESS"
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        # 'e' will contain the error object
+        print("\nERROR - (create_radclss_timeseries):" +
+              f" \n\tOccured When Saving Figure to File: \n\t{e}")
+        print(f"\tError type: {type(e)}")
+        print(f"\tLine Number: ", exc_tb.tb_lineno)
+        print(f"\tFile Name: ", exc_tb.tb_frame.f_code.co_filename)
+        print("\n")
+        STATUS = "TIMESERIES FAILED"
 
     # Clean up this function 
     del ax2, ax3, ax4
-    del gauge_precip_accum, ld_precip_accum, radar_accum
+    del radar_accum
+    if dis_site == "M1" or dis_site == "S30":
+        del ld_precip_accum
+    if dis_site == "M1":
+        del gauge_precip_accum
     del ds
 
-    return status
+    return STATUS
 
 def main(args):
     print(args)
@@ -244,11 +414,17 @@ def main(args):
         print(nvol)
         print(nvol["radclss"])
         if nvol["radclss"]:
-            status = create_radclss_timeseries(nvol["radclss"])
-            #status = radclss(nvol, outdir=OUT_PATH, serial=args.serial, postprocess=args.postproc)
+            status = create_radclss_timeseries(nvol["radclss"], 
+                                               outdir=args.outdir,
+                                               dis_site=args.station,
+                                               field=args.field)
+            print(status)
+            status = create_radclss_columns(nvol["radclss"],
+                                            outdir=args.outdir,
+                                            field=args.field)
             print(status)
  
-    print("processing finished: ", time.strftime("%H:%M:%S"))
+    print("quicklooks finished: ", time.strftime("%H:%M:%S"))
     # free up memory
     del volumes, nvol
 
@@ -304,6 +480,12 @@ if __name__ == "__main__":
                         dest="station",
                         type=str,
                         help="[str|default=M1] Specific In-Situ site to display")
+    
+    parser.add_argument("--field",
+                        default="corrected_reflectivity",
+                        dest="field",
+                        type=str,
+                        help="[str] Specific radar parameter to display")
    
     parser.add_argument("--verbose",
                         default=False,
